6a6608557a253542ba8c1c237fdbf292d88d3840
maksimis <maksimis_reg@mail.ru>
2014-06-07 18:36:03 +0400

    first

---
+++ MvcApplication1/Controllers/CourseController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class CourseController : Controller
+    {
+        //
+        // GET: /Artists/
+
+        public ActionResult Index()
+        {
+            var model = new List<CourseModel>();
+            model.Add(new CourseModel()
+            {
+                ID = 1,
+                Name = "Базовый курс английского языка для начинающих",
+                level = "Elementary",
+                Price = 0,
+                ImageSource = "0.jpg",
+                Description = "Изучение основных конструкций в английском языке: простое прошедшее, настоящее, будущее время",
+                autor = "Макс",
+                autorID = 1,
+                rating = 5
+            });
+            model.Add(new CourseModel()
+            {
+                ID = 1,
+                Name = "Продвинутый курс английского языка для начинающих",
+                level = "Elementary",
+                Price = 10,
+                ImageSource = "0.jpg",
+                Description = "Изучение продвинытых конструкций в английском языке: совершенное прошедшее, настоящее, будущее время",
+                autor = "Макс",
+                autorID = 1,
+                rating = 5
+            });
+            model.Add(new CourseModel()
+            {
+                ID = 1,
+                Name = "Базовый курс английского языка для начинающих",
+                level = "Elementary",
+                Price = 0,
+                ImageSource = "0.jpg",
+                Description = "Изучение основных конструкций в английском языке: простое прошедшее, настоящее, будущее время",
+                autor = "Макс",
+                autorID = 1,
+                rating = 5
+            });
+
+            return View(model);
+        }
+
+        public ActionResult Details (int id) {
+            var model = new CourseModel()
+            {
+                ID = 1,
+                Name = "Базовый курс английского языка для начинающих",
+                level = "Elementary",
+                Price = 0,
+                ImageSource = "0.jpg",
+                Description = "Изучение основных конструкций в английском языке: простое прошедшее, настоящее, будущее время",
+                autor = "Макс",
+                autorID = 1,
+                rating = 5
+            };
+        return View(model);
+        }
+    }
+}
---
+++ MvcApplication1/Models/CoourseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class CourseModel
+    {
+        public int ID { get; set; }
+        public string Name { set; get; }
+        public string level { set; get; }
+        public string ImageSource { set; get; }
+        public string Description { set; get; }
+        public double Price { set; get; }
+        public string autor { set; get; }
+        public int autorID { set; get; }
+        public int rating { set; get; }
+
+    }
+    
+}
